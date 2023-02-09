2d15a172ef7db731cfb4c1c89b81f069af8ee837
jm <jorgitomonti@gmail.com>
2023-02-08 21:40:20 -0300

    chore: complete example_A graphs & statistics values

--- da00_03/example_a.py
+++ da00_03/example_a.py
@@ -1,18 +1,16 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-#!head data/sales_data.csv
+#!head data/sales_data.csv - # bash-shell command 'head'
 
 sales = pd.read_csv('data/sales_data.csv', parse_dates=['Date'])
-#print(sales.head())
-print(sales.shape)
-#print(sales.info())
-#print(sales.describe())
-print(sales['Unit_Cost'].mean())
+#print(sales.head())    # .head(n=5), first 5 rows of DF w/cos titles
+#print(sales.tail())    # .tail(n=5), last 5 rows of DF w/cos titles
+#print(sales)            # print all de DF as a table (using ... for large data)
+print(sales.shape)      # (rows, cols) tuple
+#print(sales.info())    # DF structure info
+#print(sales.describe())    # DF cols statistics values
+print(sales['Unit_Cost'].mean())    # mean of Unic_Cost column
 
-import matplotlib.pyplot as plt
-import matplotlib_inline
-
-#%matplotlib inline
-
-print(sales['Unit_Cost'].plot())
-print(sales['Unit_Cost'].plot(kind='box', vert=False, figsize=(14,6)))
+sales['Unit_Cost'].plot(kind='box', vert=False, figsize=(14,6))     # box plot Unit_Cost col
+plt.show()          # necesary to display the graph
